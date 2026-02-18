dd36790f83a04edb466410550b71a64fc5fbe671
Bungkii <30260@student.act.ac.th>
2026-02-18 19:55:48 +0700

    Update mock.js

--- src/assets/mock/mock.js
+++ src/assets/mock/mock.js
@@ -1,18 +1,17 @@
 import image1 from "../images/IMG_0859.jpg";
 
-// 1. Import รูปภาพของขวัญทั้งหมดจากโฟลเดอร์ gifts
-import gift1 from "../images/gifts/31E0CB72-9BF1-4A9A-AA37-C1C0885079E3.jpg"; // (สมมติว่าเป็น .jpg นะครับ เช็คในเครื่องอีกที)
+// Import รูปของขวัญทั้งหมด
+import gift1 from "../images/gifts/31E0CB72-9BF1-4A9A-AA37-C1C0885079E3.jpg";
 import gift2 from "../images/gifts/1.png";
 import gift3 from "../images/gifts/IMG_1323.jpg";
 import gift4 from "../images/gifts/IMG_1951.jpg";
 import gift5 from "../images/gifts/IMG_1955.jpg";
 
-// ถ้า _albums จะมีหลายรูปในอนาคต แนะนำให้ทำเป็น Array ไว้เลยครับ
 export const _albums = [
   image1
 ];
 
-// 2. จับรูปทั้งหมดมาใส่ใน Array [ ]
+// รวมรูปของขวัญไว้ใน Array
 export const _giftAlbums = [
   gift1,
   gift2,
@@ -22,12 +21,12 @@ export const _giftAlbums = [
 ];
 
 export const _messages = [
-    "วุ้นมะพร้าวขาวใสใจสะอาด",
-    "แล้วไงต่อวะ",
-    "น้องน้องน้องน้องน้องน้องน้องน้องน้องน้องน้องน้อง",
+  "วุ้นมะพร้าวขาวใสใจสะอาด",
+  "แล้วไงต่อวะ",
+  "น้องน้องน้องน้องน้องน้องน้องน้องน้องน้องน้องน้อง",
 ];
 
 export const _birthdayMessages = [
-    "สุขสันต์วันเกิดครับน้องน้องน้องน้องน้องน้อง",    
-    "ราชินีหมูกรอบ",    
+  "สุขสันต์วันเกิดครับน้องน้องน้องน้องน้องน้อง",    
+  "ราชินีหมูกรอบ",    
 ];
