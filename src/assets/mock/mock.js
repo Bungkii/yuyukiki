4aea69e34a1660a803ebd4bc64896157cc91e9bc
Bungkii <30260@student.act.ac.th>
2026-02-18 19:51:38 +0700

    Update mock.js

--- src/assets/mock/mock.js
+++ src/assets/mock/mock.js
@@ -1,12 +1,25 @@
 import image1 from "../images/IMG_0859.jpg";
-import image2 from "../images/IMG_1951.jpg";
-import image3 from "../images/IMG_1955.jpg";
-import gift1 from "../images/gifts/31E0CB72-9BF1-4A9A-AA37-C1C0885079E3.jpg";
-import gift2 from "../images/IMG_1323.jpg";
 
-export const _albums = image1;
+// 1. Import รูปภาพของขวัญทั้งหมดจากโฟลเดอร์ gifts
+import gift1 from "../images/gifts/31E0CB72-9BF1-4A9A-AA37-C1C0885079E3.jpg"; // (สมมติว่าเป็น .jpg นะครับ เช็คในเครื่องอีกที)
+import gift2 from "../images/gifts/1.png";
+import gift3 from "../images/gifts/IMG_1323.jpg";
+import gift4 from "../images/gifts/IMG_1951.jpg";
+import gift5 from "../images/gifts/IMG_1955.jpg";
 
-export const _giftAlbums = gift1;
+// ถ้า _albums จะมีหลายรูปในอนาคต แนะนำให้ทำเป็น Array ไว้เลยครับ
+export const _albums = [
+  image1
+];
+
+// 2. จับรูปทั้งหมดมาใส่ใน Array [ ]
+export const _giftAlbums = [
+  gift1,
+  gift2,
+  gift3,
+  gift4,
+  gift5
+];
 
 export const _messages = [
     "วุ้นมะพร้าวขาวใสใจสะอาด",
